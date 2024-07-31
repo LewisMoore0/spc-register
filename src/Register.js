c12c3d269f36d05a784764768fc3eb74aa737708
LewisMoore0 <106735867+LewisMoore0@users.noreply.github.com>
2024-07-31 17:21:47 +0100

    Formatted date from Date component

--- src/Register.js
+++ src/Register.js
@@ -1,30 +1,17 @@
-import { Typography, Box, Button, FormControl, Input, InputLabel, TextField } from '@mui/material' 
+import { Button, InputLabel, TextField, Autocomplete} from '@mui/material' 
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { DemoContainer } from '@mui/x-date-pickers/internals/demo';
-import { useForm, FormProvider, useFormContext, Controller } from "react-hook-form"
-
-            {/* <FormProvider {...methods}>
-                <form onSubmit={methods.handleSubmit(onSubmit)}>
-                    <LocalizationProvider dateAdapter={AdapterDayjs}>
-                        <DemoContainer components={['DatePicker']}>
-                            <DatePicker label="Basic date picker" />
-                        </DemoContainer>
-                    </LocalizationProvider>
-                    <button type="submit">Submit</button>
-                </form>
-            </FormProvider> */}
-
+import { useForm, Controller } from "react-hook-form"
 
 export const Register = () => {
     const { register, getValues, control } = useForm() 
-
     const handleSubmit = (event) => {
         event.preventDefault();
         const formValues = getValues()
 
-        const formattedValues = { ...formValues, date: formValues.date.$d}
+        const formattedValues = { ...formValues, date: formValues.date.$d.toISOString()}
         console.log(formattedValues, 'formvalues');
   }
 
@@ -54,7 +41,7 @@ export const Register = () => {
                     </LocalizationProvider>
                 }
             />
-
+            
             <Button type='submit'>Submit</Button>
         </form>
     )
